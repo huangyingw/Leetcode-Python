28e4f1b986a2530f29576800f6c319a17f454132
Ying Huang <huangyingw@gmail.com>
2019-09-06 18:03:32 -0700

    n

--- 205.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        ds, dt = {}, {}
-        for i, j in zip(s, t):
-            if (i in ds and ds[i] != j) or (j in dt and dt[j] != i):
-                return False
-            ds[i] = j
-            dt[j] = i
-        return True
