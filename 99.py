ec4e5e9fbc6a7b622da144663aa918777b472033
ttian <tiant@qualtrics.com>
2020-06-28 23:37:41 -0700

    update

--- 99.py
+++ 99.py
@@ -5,9 +5,32 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# Space: O(h), where h is the tree height
+class Solution:
+    def __init__(self):
+        self.prev = TreeNode(float('-inf'))
+        self.first = None
+        self.second = None
+
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.traverse(root)
+        self.first.val, self.second.val = self.second.val, self.first.val
+
+    def traverse(self, node):
+        if not node: return
+        self.traverse(node.left)
+        if self.prev.val > node.val:
+            self.first = self.prev if not self.first else self.first
+            self.second = node
+        self.prev = node
+        self.traverse(node.right)
+
 
 # Space: O(1) with Morris Inorder traversal
-class Solution:
+class Solution1:
     def __init__(self):
         self.previous = TreeNode(float('-inf'))
         self.first = None
